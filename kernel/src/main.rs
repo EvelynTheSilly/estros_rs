98761046dbb9c398abc64240430d680cfbbe9dc1
Evelyn <45495711+EvelynTheSilly@users.noreply.github.com.>
2026-01-27 23:31:43 +0000

    implement panic function

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,7 +1,16 @@
 #![no_std]
 #![no_main]
 
+#[allow(unused)]
+use core::panic::PanicInfo;
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _kernel_entry() -> ! {
-    loop{}
+    loop {}
 }
