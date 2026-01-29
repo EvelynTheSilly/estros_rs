f9350487c236b24daee5674658cff7a6dd8cf529
Evelyn <45495711+EvelynTheSilly@users.noreply.github.com.>
2026-01-29 02:18:46 +0000

    feat allocator and youre telling me this took how long in zig???

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,18 +1,20 @@
 #![no_std]
 #![no_main]
+#![allow(unused_unsafe)]
 
-#[allow(unused)]
 use core::panic::PanicInfo;
-use cortex_a::{asm, registers::*};
-use tock_registers::interfaces::Writeable;
+
+use alloc::string::String;
+
+mod mem;
+mod uart;
+extern crate alloc;
+
 core::arch::global_asm!(include_str!("boot.S"));
 
-mod uart;
-
-//#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    println!("KERNEL PANIC");
+    println!("KERNEL PANIC: {}", { _info.message() });
     loop {}
 }
 
@@ -21,12 +23,15 @@ pub extern "C" fn _kernel_entry() -> ! {
     unsafe {
         println!("test");
         println!(
-            "rust? memory safety? speed? - you want it? it's yours my friend, as long as you have enough patience\n"
+            "rust? memory safety? speed? - you want it? it's yours my friend, as long as you have enough patience"
         );
         println!(
-            "\nsorry link, i can't give ease of use, come back when youre a little - mmmm - smarter\n"
+            "sorry link, i can't give ease of use, come back when youre a little - mmmm - smarter"
         );
-        panic!("panic test");
-        loop {}
+        let mut str = String::from("haiii im a string");
+        println!("string: {str}");
+        str.push_str(" push");
+        println!("string: {str}");
+        panic!("reached end of init function");
     };
 }
---
+++ kernel/src/mem/alloc/mod.rs
@@ -0,0 +1,56 @@
+use core::alloc::GlobalAlloc;
+use core::alloc::Layout;
+use core::cell::UnsafeCell;
+use core::ptr::null_mut;
+use core::sync::atomic::{AtomicUsize, Ordering::Relaxed};
+
+const ARENA_SIZE: usize = 128 * 1024;
+const MAX_SUPPORTED_ALIGN: usize = 4096;
+/// taken straight from the rust documentations, should work for some time
+#[repr(C, align(4096))] // 4096 == MAX_SUPPORTED_ALIGN
+struct ArenaAllocator {
+    arena: UnsafeCell<[u8; ARENA_SIZE]>,
+    remaining: AtomicUsize, // we allocate from the top, counting down
+}
+
+#[global_allocator]
+static ALLOCATOR: ArenaAllocator = ArenaAllocator {
+    arena: UnsafeCell::new([0x55; ARENA_SIZE]),
+    remaining: AtomicUsize::new(ARENA_SIZE),
+};
+
+unsafe impl Sync for ArenaAllocator {}
+
+unsafe impl GlobalAlloc for ArenaAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let size = layout.size();
+        let align = layout.align();
+
+        // `Layout` contract forbids making a `Layout` with align=0, or align not power of 2.
+        // So we can safely use a mask to ensure alignment without worrying about UB.
+        let align_mask_to_round_down = !(align - 1);
+
+        if align > MAX_SUPPORTED_ALIGN {
+            return null_mut();
+        }
+
+        let mut allocated = 0;
+        if self
+            .remaining
+            .fetch_update(Relaxed, Relaxed, |mut remaining| {
+                if size > remaining {
+                    return None;
+                }
+                remaining -= size;
+                remaining &= align_mask_to_round_down;
+                allocated = remaining;
+                Some(remaining)
+            })
+            .is_err()
+        {
+            return null_mut();
+        };
+        unsafe { self.arena.get().cast::<u8>().add(allocated) }
+    }
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
+}
