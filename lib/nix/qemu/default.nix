590c02756b8b3f7991fc87acf799b7aa2b23108e
Evelyn <45495711+EvelynTheSilly@users.noreply.github.com.>
2026-08-26 01:29:49 +0100

    fix krun/debug

--- lib/nix/qemu/default.nix
+++ lib/nix/qemu/default.nix
@@ -11,11 +11,16 @@
       ...
     }:
     let
-      kernel = self'.packages.kernel_elf;
       limine = pkgs.limine-full;
       ovmf = pkgs.pkgsCross.aarch64-multiplatform.OVMF.fd;
+      cross = pkgs.pkgsCross.aarch64-embedded;
+      isLinux = system != "aarch64-darwin";
 
-      build = import ./_build.nix { inherit pkgs kernel limine ovmf; };
+      build = import ./_build.nix {
+        pkgs = pkgs;
+        kernel = self'.packages.kernel_elf;
+        inherit limine ovmf;
+      };
       mkScript = import ./_scripts.nix;
 
       run =
@@ -31,20 +36,6 @@
           inherit (build) efiVars diskImage;
           extraFlags = "-S -s";
         };
-
-      init = self'.packages.init;
-      cross = pkgs.pkgsCross.aarch64-embedded;
-      isLinux = system != "aarch64-darwin";
-
-      gdbWrapper = pkgs.writeShellScriptBin "gdb" ''
-        tmpgdbinit=$(mktemp)
-        trap 'rm -f "$tmpgdbinit"' EXIT
-        sed \
-          -e 's|KERNEL_ELF_PATH|${kernel}/kernel.elf|g' \
-          -e 's|INIT_ELF_PATH|${init}/init.elf|g' \
-          ${./gdbinit} > "$tmpgdbinit"
-        exec ${cross.buildPackages.gdb}/bin/aarch64-none-elf-gdb -ix "$tmpgdbinit" "$@"
-      '';
     in
     {
       packages = {
@@ -52,16 +43,27 @@
         default = run;
 
         krun = pkgs.writeShellScriptBin "krun" ''
-          exec ${run}/bin/estros-run "$@"
+          exec nix run .#run -- "$@"
         '';
         kdebug = pkgs.writeShellScriptBin "kdebug" ''
-          ${pkgs.alacritty}/bin/alacritty -e ${gdbWrapper}/bin/gdb &
+          nix build .#gdb
+          ${pkgs.alacritty}/bin/alacritty -e ./result/bin/gdb &
           gdb_pid=$!
           trap 'kill $gdb_pid 2>/dev/null' EXIT
-          exec ${debug}/bin/estros-debug "$@"
+          exec nix run .#debug -- "$@"
         '';
       } // pkgs.lib.optionalAttrs isLinux {
-        gdb = gdbWrapper;
+        gdb = pkgs.writeShellScriptBin "gdb" ''
+          kernel_path=$(nix build .#kernel_elf --no-link --print-out-paths)
+          init_path=$(nix build .#init --no-link --print-out-paths)
+          tmpgdbinit=$(mktemp)
+          trap 'rm -f "$tmpgdbinit"' EXIT
+          sed \
+            -e "s|KERNEL_ELF_PATH|$kernel_path/kernel.elf|g" \
+            -e "s|INIT_ELF_PATH|$init_path/init.elf|g" \
+            ${./gdbinit} > "$tmpgdbinit"
+          exec ${cross.buildPackages.gdb}/bin/aarch64-none-elf-gdb -ix "$tmpgdbinit" "$@"
+        '';
       };
     };
 }
